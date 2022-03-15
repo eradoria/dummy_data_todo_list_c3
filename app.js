700c944168905a8bc25f8716cb452d1239eaf120
eradoria <eradoria@gmail.com>
2022-03-14 20:51:52 -0500

    1st

--- app.js
+++ app.js
@@ -26,12 +26,12 @@
     }
     
     const populateTodos = () => {
-        console.log(arrayOfTodos)
+        // console.log(arrayOfTodos)
         let ol = document.getElementById('todo-list');
         for (let index = 0; index < arrayOfTodos.length; index++) {
             const obj = arrayOfTodos[index];
-            console.log(obj);
-            console.log(obj.title);
+            // console.log(obj);
+            // console.log(obj.title);
 
             // grab an object
             let li = document.createElement('li');
@@ -39,11 +39,50 @@
             li.appendChild(title); 
             ol.appendChild(li); 
         }
-        // let li = document.createElement('li');
-        // var title = document.createTextNode(obj.title);
-        // li.appendChild(title); 
-        // ol.appendChild(li); 
     }
+
+
+    let input = document.getElementById("number")
+    
+    const chooseid = () => {
+         if (input >= 11 || input <= 0 || input === null){
+            return ;
+        }
+    }    
+
+
+    function userid() {
+    const arrayuserid = arrayOfTodos.filter(array => ({
+        userid: array.userId,
+        id: array.id,
+        title: array.title,
+        completed: array.completed
+    }))
+    console.log(arrayuserid)
+    let ol = document.getElementById('todo-list');
+        for (let index = 0; index < arrayOfTodos.length; index++) {
+            const obj = arrayOfTodos[index];
+    let li = document.createElement('li')
+    var list = document.createTextNode(obj.arrayOfTodos)
+    li.appendChild(list)
+    ol.appendChild(li)
+}}
+
+
+
+
+    // const userid = arrayuserid.map( id => {
+    //     return  })
+
+    // const list = () => {
+    //     document.getElementById("todo-list").innerHTML = arrayOfTodos1
+    // }
+
+    // const listcompleted = arrayOfTodos.filter( array => ({
+    //     if array.completed === 'true' {
+    //         return completed
+    //     }
+    // }) )
 
     const arrayOfTodos1 = [
         {
@@ -60,5 +99,5 @@
     }
     ]
 
-    console.log(arrayOfTodos[0].userId) // => 14
-    console.log(arrayOfTodos[1].userId) // => 20
+    // console.log(arrayOfTodos[0].userId) // => 14
+    // console.log(arrayOfTodos[1].userId) // => 20
